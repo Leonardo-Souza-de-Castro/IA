9bf5ebed0c6f542061d32ad693f0c49e10f1b78b
Leonardo Souza De Castro <47073656808@info.scs>
2022-02-03 11:29:42 -0300

    Projeto Quase terminado

---
+++ JARVISMEMO3/Form1.Designer.cs
@@ -0,0 +1,152 @@
+﻿
+namespace JARVISMEMO3
+{
+    partial class Form1
+    {
+        /// <summary>
+        ///  Required designer variable.
+        /// </summary>
+        private System.ComponentModel.IContainer components = null;
+
+        /// <summary>
+        ///  Clean up any resources being used.
+        /// </summary>
+        /// <param name="disposing">true if managed resources should be disposed; otherwise, false.</param>
+        protected override void Dispose(bool disposing)
+        {
+            if (disposing && (components != null))
+            {
+                components.Dispose();
+            }
+            base.Dispose(disposing);
+        }
+
+        #region Windows Form Designer generated code
+
+        /// <summary>
+        ///  Required method for Designer support - do not modify
+        ///  the contents of this method with the code editor.
+        /// </summary>
+        private void InitializeComponent()
+        {
+            this.components = new System.ComponentModel.Container();
+            this.button1 = new System.Windows.Forms.Button();
+            this.textBox1 = new System.Windows.Forms.TextBox();
+            this.tabControl1 = new System.Windows.Forms.TabControl();
+            this.tabPage1 = new System.Windows.Forms.TabPage();
+            this.tabPage2 = new System.Windows.Forms.TabPage();
+            this.textBox2 = new System.Windows.Forms.TextBox();
+            this.button2 = new System.Windows.Forms.Button();
+            this.timer1 = new System.Windows.Forms.Timer(this.components);
+            this.tabControl1.SuspendLayout();
+            this.tabPage1.SuspendLayout();
+            this.tabPage2.SuspendLayout();
+            this.SuspendLayout();
+            // 
+            // button1
+            // 
+            this.button1.Location = new System.Drawing.Point(30, 230);
+            this.button1.Name = "button1";
+            this.button1.Size = new System.Drawing.Size(182, 29);
+            this.button1.TabIndex = 0;
+            this.button1.Text = "INICIAR";
+            this.button1.UseVisualStyleBackColor = true;
+            this.button1.Click += new System.EventHandler(this.button1_Click);
+            // 
+            // textBox1
+            // 
+            this.textBox1.Location = new System.Drawing.Point(30, 23);
+            this.textBox1.Multiline = true;
+            this.textBox1.Name = "textBox1";
+            this.textBox1.Size = new System.Drawing.Size(731, 190);
+            this.textBox1.TabIndex = 1;
+            this.textBox1.TextChanged += new System.EventHandler(this.textBox1_TextChanged_1);
+            // 
+            // tabControl1
+            // 
+            this.tabControl1.Controls.Add(this.tabPage1);
+            this.tabControl1.Controls.Add(this.tabPage2);
+            this.tabControl1.Location = new System.Drawing.Point(12, 24);
+            this.tabControl1.Name = "tabControl1";
+            this.tabControl1.SelectedIndex = 0;
+            this.tabControl1.Size = new System.Drawing.Size(946, 441);
+            this.tabControl1.TabIndex = 2;
+            // 
+            // tabPage1
+            // 
+            this.tabPage1.Controls.Add(this.textBox1);
+            this.tabPage1.Controls.Add(this.button1);
+            this.tabPage1.Location = new System.Drawing.Point(4, 29);
+            this.tabPage1.Name = "tabPage1";
+            this.tabPage1.Padding = new System.Windows.Forms.Padding(3);
+            this.tabPage1.Size = new System.Drawing.Size(938, 408);
+            this.tabPage1.TabIndex = 0;
+            this.tabPage1.Text = "TEXTO PARA FALA";
+            this.tabPage1.UseVisualStyleBackColor = true;
+            // 
+            // tabPage2
+            // 
+            this.tabPage2.Controls.Add(this.textBox2);
+            this.tabPage2.Controls.Add(this.button2);
+            this.tabPage2.Location = new System.Drawing.Point(4, 29);
+            this.tabPage2.Name = "tabPage2";
+            this.tabPage2.Padding = new System.Windows.Forms.Padding(3);
+            this.tabPage2.Size = new System.Drawing.Size(938, 408);
+            this.tabPage2.TabIndex = 1;
+            this.tabPage2.Text = "FALA PARA TEXTO";
+            this.tabPage2.UseVisualStyleBackColor = true;
+            // 
+            // textBox2
+            // 
+            this.textBox2.BackColor = System.Drawing.SystemColors.HighlightText;
+            this.textBox2.Location = new System.Drawing.Point(36, 96);
+            this.textBox2.Multiline = true;
+            this.textBox2.Name = "textBox2";
+            this.textBox2.Size = new System.Drawing.Size(749, 253);
+            this.textBox2.TabIndex = 1;
+            // 
+            // button2
+            // 
+            this.button2.Location = new System.Drawing.Point(36, 35);
+            this.button2.Name = "button2";
+            this.button2.Size = new System.Drawing.Size(294, 29);
+            this.button2.TabIndex = 0;
+            this.button2.Text = "OUVIR";
+            this.button2.UseVisualStyleBackColor = true;
+            this.button2.Click += new System.EventHandler(this.button2_Click);
+            // 
+            // timer1
+            // 
+            this.timer1.Enabled = true;
+            this.timer1.Tick += new System.EventHandler(this.textBox1_TextChanged_1_borges);
+            // 
+            // Form1
+            // 
+            this.AutoScaleDimensions = new System.Drawing.SizeF(8F, 20F);
+            this.AutoScaleMode = System.Windows.Forms.AutoScaleMode.Font;
+            this.ClientSize = new System.Drawing.Size(999, 743);
+            this.Controls.Add(this.tabControl1);
+            this.Name = "Form1";
+            this.Text = "Form1";
+            this.tabControl1.ResumeLayout(false);
+            this.tabPage1.ResumeLayout(false);
+            this.tabPage1.PerformLayout();
+            this.tabPage2.ResumeLayout(false);
+            this.tabPage2.PerformLayout();
+            this.ResumeLayout(false);
+
+        }
+
+        #endregion
+
+        private System.Windows.Forms.Button button1;
+        public System.Windows.Forms.TextBox textBox1;
+        private System.Windows.Forms.TabControl tabControl1;
+        private System.Windows.Forms.TabPage tabPage1;
+        private System.Windows.Forms.TabPage tabPage2;
+        private System.Windows.Forms.Button button2;
+        public System.Windows.Forms.TextBox textBox2;
+        public System.Windows.Forms.Timer timer1;
+    }
+}
+
---
+++ JARVISMEMO3/Form1.cs
@@ -0,0 +1,121 @@
+﻿using Microsoft.CognitiveServices.Speech;
+using Microsoft.CognitiveServices.Speech.Audio;
+using System;
+using System.Collections.Generic;
+using System.ComponentModel;
+using System.Data;
+using System.Drawing;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+using System.Windows.Forms;
+
+namespace JARVISMEMO3
+{
+    public partial class Form1 : Form
+    {
+        public Form1()
+        {
+            InitializeComponent();
+        }
+
+        static string borges = "";
+        static string borgesparaaudio = "";
+
+
+        private async void button1_Click(object sender, EventArgs e)
+        {
+            await SynthesizeAudioAsync();
+        }
+
+
+        private void textBox1_TextChanged(object sender, EventArgs e)
+        {
+            //if (!textBox1.Text.StartsWith("£"))
+            //{
+            //    textBox1.Text = string.Concat("£", textBox1.Text);
+            //    textBox1.Select(textBox1.Text.Length, 0);
+            //}
+
+            borges = textBox1.Text;
+
+        }
+
+        static async Task SynthesizeAudioAsync()
+        {
+
+
+
+
+            var config = SpeechConfig.FromSubscription("60997aa2668846c6a7e553b121d271a3", "brazilsouth");
+            config.SpeechSynthesisLanguage = "pt-BR";
+            using var synthesizer = new SpeechSynthesizer(config);
+            await synthesizer.SpeakTextAsync(borges);
+
+        }
+
+        private void textBox1_TextChanged_1(object sender, EventArgs e)
+        {
+
+            borges = textBox1.Text;
+        }
+
+        private async void button2_Click(object sender, EventArgs e)
+        {
+            Application.DoEvents();
+            var speechConfig = SpeechConfig.FromSubscription("60997aa2668846c6a7e553b121d271a3", "brazilsouth");
+            speechConfig.EnableDictation();
+            await FromMic(speechConfig);
+        }
+
+
+        async static Task FromMic(SpeechConfig speechConfig)
+        {
+            Application.DoEvents();
+            using var audioConfig = AudioConfig.FromDefaultMicrophoneInput();
+            using var recognizer = new SpeechRecognizer(speechConfig, "pt-BR", audioConfig);
+
+            // Console.WriteLine("Fale no microfone");
+            while (true)
+            {
+                var result = await recognizer.RecognizeOnceAsync();
+
+                borgesparaaudio = result.Text;
+
+
+
+                Form1 form1 = new Form1();
+                form1.borges2(borgesparaaudio);
+
+
+                // Console.WriteLine($"RECOGNIZED: Text={result.Text}");
+            }
+        }
+
+
+
+        private void borges2(string valor)
+        {
+
+            Application.DoEvents();
+
+            textBox2.Text = borgesparaaudio;
+
+            Application.DoEvents();
+
+
+
+        }
+
+
+        private void textBox1_TextChanged_1_borges(object sender, EventArgs e)
+        {
+            textBox2.Text = borgesparaaudio;
+            Application.DoEvents();
+        }
+
+
+
+
+    }
+}
